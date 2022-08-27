2250ee1a7e3d3595a103a04dbd53ad6dc541163c
Allan Carvalho <allan.m.carvalho@outlook.com>
2022-08-27 11:46:17 -0300

    feat(Some new components):

---
+++ resources/js/Components/Table/Table.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="table-responsive">
+        <TableStructure
+            :columns="payload.columns"
+            :rows="payload.rows"/>
+    </div>
+</template>
+
+<script>
+import TableStructure from "./TableStructure.vue";
+export default {
+  name: "Table",
+  components: {TableStructure},
+  props: {
+    payload: {
+      type: Object,
+      required: true
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/Table/TableStructure.vue
@@ -0,0 +1,43 @@
+<template>
+
+  <table class="table table-bordered table-striped table-hover">
+    <thead>
+    <tr>
+      <th class="bg-primary text-light" v-for="column in columns" :key="column.name">
+        {{ column.label }}
+      </th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr v-for="row in rows" :key="row.primary_key">
+      <td v-for="column in columns" :key="column.name">
+        {{ row[column.name] }}
+      </td>
+    </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "TableStructure",
+  props: {
+    columns: {
+      type: [Array, Object],
+      required: true
+    },
+    rows: {
+      type: [Array, Object],
+      required: true
+    }
+  },
+  mounted() {
+    console.log(this.columns)
+    console.log(this.rows)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
