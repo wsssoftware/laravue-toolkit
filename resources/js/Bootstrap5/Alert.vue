8a9cf0fb5ed8733f64d2f639855592ce4c77f837
Allan Carvalho <allan.m.carvalho@outlook.com>
2022-09-06 17:04:54 -0300

    feat(Worked on dropbox image system and Notifications):

---
+++ resources/js/Bootstrap5/Alert.vue
@@ -0,0 +1,50 @@
+<template>
+    <div :class="getClass"
+         role="alert">
+        <h4 v-if="$slots.header" class="alert-heading">
+            <slot name="header"/>
+        </h4>
+        <slot/>
+
+
+        <button v-if="dismissible" type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Alert",
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+        dismissible: {
+            type: Boolean,
+            default: false
+        },
+    },
+    mounted() {
+
+    },
+    computed: {
+        getClass() {
+            let type = this.type;
+            if (!type.includes('alert-')) {
+                type = 'alert-' + type;
+            }
+            return [
+                'alert',
+                type,
+                {'alert-dismissible': this.dismissible},
+                {fade: this.dismissible},
+                {show: this.dismissible},
+            ];
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/Datetime.vue
@@ -0,0 +1,34 @@
+<template>
+  <time :datetime="timestamp">
+    {{ formatted }}
+  </time>
+</template>
+
+<script>
+
+export default {
+  name: "Datetime",
+  props: {
+    datetime: {
+      required: true,
+      type: String,
+    },
+  },
+  computed: {
+    timestamp() {
+      return new Date(this.datetime).toISOString().slice(0, 10);
+    },
+    formatted() {
+      return new Date(this.datetime).toLocaleTimeString('pt-BR', {
+        year: 'numeric',
+        month: "short",
+        day: "numeric"
+      });
+    },
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
