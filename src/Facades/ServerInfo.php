ddae0ec0a3be0d3f02bc33311407c97334300ac2
Allan Carvalho <allan.m.carvalho@outlook.com>
2022-09-14 20:10:33 -0300

    feat(Created server info):

--- src/Facades/ServerInfo.php
+++ src/Facades/ServerInfo.php
@@ -2,15 +2,26 @@
 
 namespace Laravue\Facades;
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static string cpuLoad()
+ * @method static float cpuLoad()
+ * @method static float cpuLoad5min()
+ * @method static float cpuLoad15min()
+ * @method static Carbon booted()
+ * @method static float memoryUsage()
+ * @method static float memoryTotal()
+ * @method static float swapTotal()
+ * @method static float swapUsage()
+ * @method static float storageTotal()
+ * @method static float storageUsage()
  *
  * @see \Laravue\Utility\ServerInfo
  */
 class ServerInfo extends Facade
 {
+
     /**
      * {@inheritDoc}
      */
--- src/Utility/ServerInfo.php
+++ src/Utility/ServerInfo.php
@@ -2,6 +2,12 @@
 
 namespace Laravue\Utility;
 
+use Carbon\Carbon;
+use Illuminate\Support\Arr;
+use Linfo\OS\Darwin;
+use Linfo\OS\Linux;
+use Linfo\OS\Windows;
+
 /**
  * Class ServerStatus
  *
@@ -10,11 +16,125 @@ namespace Laravue\Utility;
 class ServerInfo
 {
 
-    public function cpuLoad()
-    {
-        $load = sys_getloadavg();
-        //$load = $load[0] / 2;
+    /**
+     * @var Darwin|Linux|Windows
+     */
+    protected Darwin|Linux|Windows $parser;
 
-        return shell_exec('free');
+    /**
+     *
+     */
+    public function __construct()
+    {
+        $linfo = new \Linfo\Linfo();
+        $this->parser = $linfo->getParser();
+
+    }
+
+    /**
+     * @return float
+     */
+    public function cpuLoad(): float
+    {
+        $loadNow = floatval(Arr::get($this->parser->getLoad(), 'now', 0));
+        $coreCount = count($this->parser->getCPU());
+
+        return round(($loadNow / $coreCount) * 100, 2);
+    }
+
+    /**
+     * @return float
+     */
+    public function cpuLoad5min(): float
+    {
+        $loadNow = floatval(Arr::get($this->parser->getLoad(), '5min', 0));
+        $coreCount = count($this->parser->getCPU());
+
+        return round(($loadNow / $coreCount) * 100, 2);
+    }
+
+    /**
+     * @return float
+     */
+    public function cpuLoad15min(): float
+    {
+        $loadNow = floatval(Arr::get($this->parser->getLoad(), '15min', 0));
+        $coreCount = count($this->parser->getCPU());
+
+        return round(($loadNow / $coreCount) * 100, 2);
+    }
+
+    /**
+     * @return Carbon
+     */
+    public function booted(): Carbon
+    {
+        return Carbon::createFromTimestamp(intval(Arr::get($this->parser->getUpTime(), 'bootedTimestamp', 0)));
+    }
+
+    /**
+     * @return float
+     */
+    public function memoryTotal(): float
+    {
+        return floatval(Arr::get($this->parser->getRam(), 'total', 0));
+    }
+
+    /**
+     * @return float
+     */
+    public function memoryUsage(): float
+    {
+        return $this->memoryTotal() - floatval(Arr::get($this->parser->getRam(), 'free', 0));
+    }
+
+    /**
+     * @return float
+     */
+    public function swapTotal(): float
+    {
+        return floatval(Arr::get($this->parser->getRam(), 'swapTotal', 0));
+    }
+
+    /**
+     * @return float
+     */
+    public function swapUsage(): float
+    {
+        return $this->swapTotal() - floatval(Arr::get($this->parser->getRam(), 'swapFree', 0));
+    }
+
+    /**
+     * @return float
+     */
+    public function storageTotal(): float
+    {
+        $total = 0;
+        $counted = [];
+        foreach ($this->parser->getMounts() as $mount) {
+            $size = floatval(Arr::get($mount, 'size', 0));
+            if (!in_array($size, $counted)) {
+                $total += $size;
+                $counted[] = $size;
+            }
+        }
+        return $total;
+    }
+
+    /**
+     * @return float
+     */
+    public function storageUsage(): float
+    {
+        $total = 0;
+        $counted = [];
+        foreach ($this->parser->getMounts() as $mount) {
+            $size = floatval(Arr::get($mount, 'used', 0));
+            if (!in_array($size, $counted)) {
+                $total += $size;
+                $counted[] = $size;
+            }
+        }
+        return $total;
     }
 }
